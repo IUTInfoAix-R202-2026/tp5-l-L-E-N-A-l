db84fdc96e9a8ef1561893b1017a44dfdd87a227
l-L-E-N-A-l <lena.fougeron@etu.univ-amu.fr>
2026-06-03 12:39:54 +0000

    Tous les tests passent

--- src/main/java/fr/univ_amu/iut/exercice2/BaseDeDonnees.java
+++ src/main/java/fr/univ_amu/iut/exercice2/BaseDeDonnees.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.sql.DataSource;
+import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteDataSource;
 
 /**
  * Exercice 2 : fournir une {@link DataSource} SQLite et initialiser le schéma.
@@ -33,15 +35,16 @@ public class BaseDeDonnees {
    * @param chemin chemin du fichier SQLite (ex : {@code "chauves_souris.db"})
    */
   public static DataSource surFichier(String chemin) {
+
     DataSource source = null;
 
-    // TODO exercice 2 : créer et configurer la DataSource SQLite, et l'affecter à `source`.
-    //
-    // 1. SQLiteConfig config = new SQLiteConfig();
-    //    config.enforceForeignKeys(true);   // SQLite n'applique les FK que si on le demande
-    // 2. SQLiteDataSource sqlite = new SQLiteDataSource(config);
-    //    sqlite.setUrl("jdbc:sqlite:" + chemin);
-    // 3. source = sqlite;
+    SQLiteConfig config = new SQLiteConfig();
+    config.enforceForeignKeys(true);
+
+    SQLiteDataSource sqlite = new SQLiteDataSource(config);
+    sqlite.setUrl("jdbc:sqlite:" + chemin);
+
+    source = sqlite;
 
     return source;
   }
